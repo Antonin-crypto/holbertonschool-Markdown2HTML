886acc82e72b4016a9d3855f58fcf022805a3a23
Antonin-crypto <akamis53@gmail.com>
2024-12-05 09:23:00 +0100

    pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 markdown2html.py
-A script to convert a Markdown file to an HTML file without using external libraries.
+A script to convert a Markdown file to an HTML file without using external
+libraries.
 
 Usage:
     ./markdown2html.py input_file.md output_file.html
@@ -17,7 +18,8 @@ def main():
     """
     # Check the number of arguments
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
 
     # Assign arguments
